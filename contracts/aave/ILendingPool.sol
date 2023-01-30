1e8eaf1f6888a07a30784f7063836d7fbdc9fa35
eerkaijun <kaijuneer@gmail.com>
2023-01-30 15:50:19 +0800

    add withdraw function for aave

--- contracts/aave/ILendingPool.sol
+++ contracts/aave/ILendingPool.sol
@@ -22,6 +22,23 @@ interface ILendingPool {
     ) external;
 
     /**
+     * @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+     * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+     * @param asset The address of the underlying asset to withdraw
+     * @param amount The underlying amount to be withdrawn
+     *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+     * @param to Address that will receive the underlying, same as msg.sender if the user
+     *   wants to receive it on his own wallet, or a different address if the beneficiary is a
+     *   different wallet
+     * @return The final amount withdrawn
+    **/
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+
+    /**
      * @dev Allows users to borrow a specific `amount` of the reserve underlying asset, provided that the borrower
      * already deposited enough collateral, or he was given enough allowance by a credit delegator on the
      * corresponding debt token (StableDebtToken or VariableDebtToken)

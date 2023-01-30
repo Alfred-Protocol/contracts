4879218d738fd52a93d8677ddf19159152ac03a6
eerkaijun <kaijuneer@gmail.com>
2023-01-30 14:49:00 +0800

    setup hardhat

---
+++ contracts/Funds.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract Funds {
+
+    IERC20Metadata stablecoin;
+    uint256 totalValueLocked;
+    uint256 startDate;
+    uint256 matureDate;
+
+    modifier beforeStartDate() {
+        _;
+    }
+
+    constructor(address _stablecoinAddress, uint256 _startDate, uint256 _matureDate) {
+        stablecoin = IERC20Metadata(_stablecoinAddress);
+        startDate = _startDate;
+        matureDate = _matureDate;
+    }
+
+    function deposit(uint256 _amount) public {
+        totalValueLocked += _amount;
+        stablecoin.transferFrom(msg.sender, address(this), _amount);
+    }
+
+}

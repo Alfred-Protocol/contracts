ce58188f908901d7f9ac5c604fac2f0ded98a697
eerkaijun <kaijuneer@gmail.com>
2023-01-31 11:47:56 +0800

    wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,
+      }
+    }
+  }
 };

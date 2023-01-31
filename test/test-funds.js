0a4e9910481b0fadbcbdff269573a132d25f10af
eerkaijun <kaijuneer@gmail.com>
2023-01-31 17:21:07 +0800

    some test

--- test/test-funds.js
+++ test/test-funds.js
@@ -6,7 +6,7 @@ describe("Funds Factory", function() {
     let fundsFactory;
     let uniswapAdapter;
     let funds;
-    let stablecoin, stablecoinDecimals;
+    let stablecoin, stablecoinDecimals, stablecoinAddress;
 
     beforeEach(async function() {
         [assetManager, user] = await ethers.getSigners();
@@ -18,7 +18,7 @@ describe("Funds Factory", function() {
         // uniswap v3 router address passed as argument
         uniswapAdapter = await UniswapAdapter.deploy("0xE592427A0AEce92De3Edee1F18E0157C05861564");
 
-        const stablecoinAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC Ethereum mainnet address
+        stablecoinAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC Ethereum mainnet address
         stablecoin = await ethers.getContractAt("IERC20Metadata", stablecoinAddress);
         stablecoinDecimals = await stablecoin.decimals();
         
@@ -43,14 +43,38 @@ describe("Funds Factory", function() {
     
     it("User should be able to deposit stablecoin", async function() {
         const depositAmount = ethers.utils.parseUnits("1000", stablecoinDecimals);
-        await stablecoin.connect(user).approve(funds.address, depositAmount);
-        
         const stablecoinBalanceBefore = await stablecoin.balanceOf(user.address);
-        await funds.connect(user).deposit(depositAmount);
+        await depositToFundsContract(stablecoin, funds, user, depositAmount);
         const stablecoinBalanceAfter = await stablecoin.balanceOf(user.address);
 
         expect(await funds.totalValueLocked()).to.equal(depositAmount);
         expect(await funds.depositedAmount(user.address)).to.equal(depositAmount);
         expect(stablecoinBalanceBefore.sub(stablecoinBalanceAfter)).to.equal(depositAmount);
     })
+
+    it("Asset manager should be able to supply and borrow from Aave", async function() {
+        const depositAmount = ethers.utils.parseUnits("1000", stablecoinDecimals);
+        await depositToFundsContract(stablecoin, funds, user, depositAmount);
+
+        // supply USDC to aave
+        await funds.connect(assetManager).supplyToAave(stablecoinAddress, depositAmount);
+
+        // aToken of USDC
+        const usdcAToken = await ethers.getContractAt("IERC20Metadata", "0xBcca60bB61934080951369a648Fb03DF4F96263C");
+        // funds contract should accumulate equal amount of usdc aToken
+        expect(await usdcAToken.balanceOf(funds.address)).to.equal(depositAmount);
+
+        const fraxAddress = "0x853d955acef822db058eb8505911ed77f175b99e"
+        await funds.borrowFromAave(fraxAddress, ethers.utils.parseEther("5"));
+
+        // debt token of FRAX
+        const fraxDebtToken = await ethers.getContractAt("IERC20Metadata", "0x3916e3B6c84b161df1b2733dFfc9569a1dA710c2");
+        // funds contract should accumulate equal amount of aave debt token
+        console.log(await fraxDebtToken.balanceOf(funds.address))
+    })
 })
+
+async function depositToFundsContract(stablecoin, funds, user, depositAmount) {
+    await stablecoin.connect(user).approve(funds.address, depositAmount);
+    await funds.connect(user).deposit(depositAmount);
+}

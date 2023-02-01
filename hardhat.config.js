f98eaa743b69feefd6ffd510b12bc9b2521efcd5
eerkaijun <kaijuneer@gmail.com>
2023-02-01 17:06:46 +0800

    deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,6 +14,20 @@ module.exports = {
     },
   },
   networks: {
+    avax: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_GOERLI}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
     hardhat: {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,

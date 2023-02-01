a5758f0c2d1b579702173fe9328a00bc204b4437
Matthew <alexandermatthew12@gmail.com>
2023-02-01 16:51:57 +0800

    Write tests for uniswap

--- contracts/Funds.sol
+++ contracts/Funds.sol
@@ -1,21 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import { LendingPoolAddressesProvider } from "./aave/ILendingPoolAddressesProvider.sol";
-import { ILendingPool } from "./aave/ILendingPool.sol";
-import { IFunds } from "./interfaces/IFunds.sol";
-import { UniswapAdapter } from "./uniswap/Adapter.sol";
-import {
-    FundHasStarted,
-    FundHasEnded,
-    FundHasNotEnded
-} from "./interfaces/Errors.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {LendingPoolAddressesProvider} from "./aave/ILendingPoolAddressesProvider.sol";
+import {ILendingPool} from "./aave/ILendingPool.sol";
+import {IFunds} from "./interfaces/IFunds.sol";
+import {Swap} from "./uniswap/Swap.sol";
+import {FundHasStarted, FundHasEnded, FundHasNotEnded} from "./interfaces/Errors.sol";
 
 contract Funds is IFunds {
-
     IERC20Metadata stablecoin;
-    UniswapAdapter adapter;
+    Swap adapter;
     uint256 totalValueLocked;
     uint256 startDate;
     uint256 matureDate;
@@ -56,7 +51,7 @@ contract Funds is IFunds {
         stablecoin = IERC20Metadata(_stablecoinAddress);
         startDate = _startDate;
         matureDate = _matureDate;
-        adapter = UniswapAdapter(_uniswapAdapterAddress);
+        adapter = Swap(_uniswapAdapterAddress);
     }
 
     function deposit(uint256 _amount) public beforeStartDate {
@@ -66,56 +61,100 @@ contract Funds is IFunds {
     }
 
     function withdraw() public afterEndDate {
-        uint256 entitledAmount = depositedAmount[msg.sender] * totalStablecoinAfterUnwind / totalValueLocked;
+        uint256 entitledAmount = (depositedAmount[msg.sender] *
+            totalStablecoinAfterUnwind) / totalValueLocked;
         totalValueLocked -= depositedAmount[msg.sender];
         depositedAmount[msg.sender] = 0;
         stablecoin.transfer(msg.sender, entitledAmount);
     }
 
-    function unwindAllPositions() public afterEndDate {
+    function unwindAllPositions() public afterEndDate {}
 
-    }
-
-    function supplyToAave(address _underlyingAssetAddress, uint256 _amount) public {
+    function supplyToAave(address _underlyingAssetAddress, uint256 _amount)
+        public
+    {
         // get lending pool address (Ethereum mainnet)
-        address lendingPoolAddress = LendingPoolAddressesProvider(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5).getLendingPool();
+        address lendingPoolAddress = LendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        ).getLendingPool();
 
         // approve the lending pool to transfer the underlying asset
-        IERC20Metadata(_underlyingAssetAddress).approve(lendingPoolAddress, _amount);
-        
+        IERC20Metadata(_underlyingAssetAddress).approve(
+            lendingPoolAddress,
+            _amount
+        );
+
         // this contract will receive the associated aToken
-        ILendingPool(lendingPoolAddress).deposit(_underlyingAssetAddress, _amount, address(this), 0);
+        ILendingPool(lendingPoolAddress).deposit(
+            _underlyingAssetAddress,
+            _amount,
+            address(this),
+            0
+        );
     }
 
-    function withdrawFromAave(address _underlyingAssetAddress, uint256 _amount) public {
+    function withdrawFromAave(address _underlyingAssetAddress, uint256 _amount)
+        public
+    {
         // get lending pool address (Ethereum mainnet)
-        address lendingPoolAddress = LendingPoolAddressesProvider(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5).getLendingPool();
-        
+        address lendingPoolAddress = LendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        ).getLendingPool();
+
         // this contract will receive the associated aToken
-        ILendingPool(lendingPoolAddress).withdraw(_underlyingAssetAddress, _amount, address(this));
+        ILendingPool(lendingPoolAddress).withdraw(
+            _underlyingAssetAddress,
+            _amount,
+            address(this)
+        );
     }
 
-    function borrowFromAave(address _underlyingAssetAddress, uint256 _amount) public {
+    function borrowFromAave(address _underlyingAssetAddress, uint256 _amount)
+        public
+    {
         // get lending pool address (Ethereum mainnet)
-        address lendingPoolAddress = LendingPoolAddressesProvider(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5).getLendingPool();
+        address lendingPoolAddress = LendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        ).getLendingPool();
 
         // borrow cryptoassets from lending pool - use stable interest rate
-        ILendingPool(lendingPoolAddress).borrow(_underlyingAssetAddress, _amount, 1, 0, address(this)); 
+        ILendingPool(lendingPoolAddress).borrow(
+            _underlyingAssetAddress,
+            _amount,
+            1,
+            0,
+            address(this)
+        );
     }
 
-    function repayToAave(address _underlyingAssetAddress, uint256 _amount) public {
+    function repayToAave(address _underlyingAssetAddress, uint256 _amount)
+        public
+    {
         // get lending pool address (Ethereum mainnet)
-        address lendingPoolAddress = LendingPoolAddressesProvider(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5).getLendingPool();
+        address lendingPoolAddress = LendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        ).getLendingPool();
 
         // allow lending pool to burn debt token
-        IERC20Metadata(_underlyingAssetAddress).approve(lendingPoolAddress, _amount);
+        IERC20Metadata(_underlyingAssetAddress).approve(
+            lendingPoolAddress,
+            _amount
+        );
 
         // repay borrow amount
-        ILendingPool(lendingPoolAddress).repay(_underlyingAssetAddress, _amount, 1, address(this));
+        ILendingPool(lendingPoolAddress).repay(
+            _underlyingAssetAddress,
+            _amount,
+            1,
+            address(this)
+        );
     }
 
-    function swapTokens(address _from, address _to, uint256 _amount) public {
+    function swapTokens(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public {
         adapter.swap(_from, _to, _amount);
     }
-
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,25 @@
-require('@nomicfoundation/hardhat-toolbox')
+require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: '0.8.15',
-    settings: {
-      viaIR: true,
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-}
+	solidity: {
+		version: "0.8.15",
+		settings: {
+			viaIR: true,
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	networks: {
+		hardhat: {
+			forking: {
+				url: `https://eth-mainnet.alchemyapi.io/v2/rOiQsk0cIqALCVVNiqeBhir4vk388VC_`,
+			},
+		},
+	},
+	mocha: {
+		timeout: 40000,
+	},
+};

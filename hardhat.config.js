88ede06732fda4e855cee5b24e2e4f232ed112d8
eerkaijun <kaijuneer@gmail.com>
2023-02-02 12:11:15 +0800

    produce signed message

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,8 +30,8 @@ module.exports = {
     },
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,
-        //url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_GOERLI}`,
+        //url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_GOERLI}`,
       }
     }
   }
--- test/test-wormhole.js
+++ test/test-wormhole.js
@@ -1,15 +1,26 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const {CHAIN_ID_ETH, CHAIN_ID_AVAX, tryNativeToHexString} = require("@certusone/wormhole-sdk");
+const { formatWormholeMessageFromReceipt } = require("../utils/helpers");
+const { MockGuardians } = require("@certusone/wormhole-sdk/lib/cjs/mock");
 
 describe("Wormhole Integration", function() {
 
     let depositContract;
     let stablecoin;
     let assetManager;
+    let guardians;
 
     beforeEach(async function() {
         [assetManager] = await ethers.getSigners();
+
+        const AVAX_WORMHOLE_GUARDIAN_SET_INDEX = 3
+        const GUARDIAN_PRIVATE_KEY = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0"
+
+        guardians = new MockGuardians(
+            AVAX_WORMHOLE_GUARDIAN_SET_INDEX, 
+            [GUARDIAN_PRIVATE_KEY]
+        );
 
         console.log("Chain ID AVAX: ", CHAIN_ID_AVAX);
         console.log("Chain ID ETH: ", CHAIN_ID_ETH);
@@ -21,7 +32,7 @@ describe("Wormhole Integration", function() {
         stablecoin = await ethers.getContractAt("IERC20Metadata", "0x78deca24cba286c0f8d56370f5406b48cfce2f86");
     })
     
-    it.only("Should be able to bridge USDC from AVAX to ETH", async function() {
+    it.only("Should be able to bridge USDC from ETH to AVAX", async function() {
 
         const stablecoinDecimals = await stablecoin.decimals();
         console.log("Stablecoin decimals: ", stablecoinDecimals);
@@ -32,7 +43,7 @@ describe("Wormhole Integration", function() {
         const usdcWhale = await ethers.provider.getSigner('0xeeed1866e82808d6035372e4fd51455ea520e69a');
         await stablecoin.connect(usdcWhale).transfer(assetManager.address, ethers.utils.parseUnits("10000", stablecoinDecimals));
 
-        console.log("Stablecoin balance of asset manager: ", await stablecoin.balanceOf(assetManager.address));
+        const senderStablcoinBalanceBefore = await stablecoin.balanceOf(assetManager.address);
 
         // recipient address on avax
         const avaxAddress = "0x02726D50CD200C195F3D1Cd5349Ca1B7f90D6BB5";
@@ -41,12 +52,32 @@ describe("Wormhole Integration", function() {
         console.log("Chain ID ETH: ", CHAIN_ID_ETH);
 
         await stablecoin.approve(depositContract.address, transferAmount);
-        await depositContract.sendTokensWithPayload(
+        const tx = await depositContract.sendTokensWithPayload(
             stablecoin.address,
             transferAmount,
             CHAIN_ID_AVAX, // targetChainId
             0, // batchId=0 to opt out of batching
             "0x" + tryNativeToHexString(avaxAddress, CHAIN_ID_AVAX)
-        )  
+        )
+        const receipt = await tx.wait()
+        //console.log("Transaction receipt: ", receipt);
+
+        const senderStablcoinBalanceAfter = await stablecoin.balanceOf(assetManager.address);
+        expect(senderStablcoinBalanceBefore.sub(senderStablcoinBalanceAfter)).to.equal(transferAmount);
+
+        // now grab the Wormhole message
+        const unsignedMessages = await formatWormholeMessageFromReceipt(
+            receipt,
+            CHAIN_ID_ETH
+        );
+        expect(unsignedMessages.length).to.equal(1);
+
+        // sign the TransferWithPayload message
+        const signedTransferMessage = Uint8Array.from(
+            guardians.addSignatures(unsignedMessages[0], [0])
+        );
+
+        console.log("Signed messages: ", signedTransferMessage)
+
     })
 })

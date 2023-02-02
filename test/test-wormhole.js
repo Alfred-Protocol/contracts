0113bac9f1028a0ac5b06d1030450313fd446f96
eerkaijun <kaijuneer@gmail.com>
2023-02-02 16:00:20 +0800

    redeem tests

--- test/test-wormhole.js
+++ test/test-wormhole.js
@@ -1,10 +1,11 @@
 const { expect } = require("chai");
+const fs = require('fs');
 const { ethers } = require("hardhat");
 const {CHAIN_ID_ETH, CHAIN_ID_AVAX, tryNativeToHexString} = require("@certusone/wormhole-sdk");
 const { formatWormholeMessageFromReceipt } = require("../utils/helpers");
 const { MockGuardians } = require("@certusone/wormhole-sdk/lib/cjs/mock");
 
-describe("Wormhole Integration", function() {
+describe("Wormhole Integration (ETH side)", function() {
 
     let depositContract;
     let stablecoin;
@@ -29,18 +30,18 @@ describe("Wormhole Integration", function() {
         depositContract = await ethers.getContractAt("HelloToken", "0x10219b965C8272245e3A08FBbB692539558FcaC8");
 
         // USDC stablecoin address (on Ethereum)
-        stablecoin = await ethers.getContractAt("IERC20Metadata", "0x78deca24cba286c0f8d56370f5406b48cfce2f86");
+        stablecoin = await ethers.getContractAt("IERC20Metadata", "0x07865c6E87B9F70255377e024ace6630C1Eaa37F");
     })
     
-    it.only("Should be able to bridge USDC from ETH to AVAX", async function() {
+    it("Should be able to bridge USDC from ETH to AVAX", async function() {
 
         const stablecoinDecimals = await stablecoin.decimals();
         console.log("Stablecoin decimals: ", stablecoinDecimals);
 
         // get a USDC whale to transfer some USDC to the user (for mainnet forking test)
         const transferAmount = ethers.utils.parseUnits("100", stablecoinDecimals)
-        await ethers.provider.send('hardhat_impersonateAccount', ['0xeeed1866e82808d6035372e4fd51455ea520e69a']);
-        const usdcWhale = await ethers.provider.getSigner('0xeeed1866e82808d6035372e4fd51455ea520e69a');
+        await ethers.provider.send('hardhat_impersonateAccount', ['0x797c7ab9a2a29089b643e0b97d70fab7d2a07ddd']);
+        const usdcWhale = await ethers.provider.getSigner('0x797c7ab9a2a29089b643e0b97d70fab7d2a07ddd');
         await stablecoin.connect(usdcWhale).transfer(assetManager.address, ethers.utils.parseUnits("10000", stablecoinDecimals));
 
         const senderStablcoinBalanceBefore = await stablecoin.balanceOf(assetManager.address);
@@ -66,6 +67,52 @@ describe("Wormhole Integration", function() {
         expect(senderStablcoinBalanceBefore.sub(senderStablcoinBalanceAfter)).to.equal(transferAmount);
 
         // now grab the Wormhole message
+        const json = JSON.stringify(receipt)
+        fs.writeFile('output.json', json, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('File saved successfully');
+            }
+        });
+        const unsignedMessages = await formatWormholeMessageFromReceipt(
+            receipt,
+            CHAIN_ID_ETH
+        );
+        expect(unsignedMessages.length).to.equal(1);
+
+        // sign the TransferWithPayload message
+        const signedTransferMessage = Uint8Array.from(
+            guardians.addSignatures(unsignedMessages[0], [0])
+        );
+
+        console.log("Signed messages: ", signedTransferMessage)
+    })
+
+})
+
+describe.only("Wormhold integration (AVAX side)", function() {
+
+    let receipt;
+
+    const AVAX_WORMHOLE_GUARDIAN_SET_INDEX = 3
+    const GUARDIAN_PRIVATE_KEY = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0"
+
+    guardians = new MockGuardians(
+        AVAX_WORMHOLE_GUARDIAN_SET_INDEX, 
+        [GUARDIAN_PRIVATE_KEY]
+    );
+
+    fs.readFile('output.json', 'utf-8', (err, data) => {
+        if (err) {
+           console.error(err);
+        } else {
+           receipt = JSON.parse(data);
+        }
+    });
+
+    it("Redeem USDC on AVAX", async function() {
+        console.log("Receipt: ", receipt);
         const unsignedMessages = await formatWormholeMessageFromReceipt(
             receipt,
             CHAIN_ID_ETH
@@ -79,5 +126,19 @@ describe("Wormhole Integration", function() {
 
         console.log("Signed messages: ", signedTransferMessage)
 
+        // USDC stablecoin address (on Avax Fuji)
+        stablecoin = await ethers.getContractAt("IERC20Metadata", "0x5425890298aed601595a70AB815c96711a31Bc65")
+
+        // token contract on Fuji
+        depositContract = await ethers.getContractAt("HelloToken", "0x6e1ef01273DbB1e99311bde7467512165f16DB78")
+
+        const receiverStablecoinBalanceBefore = await stablecoin.balanceOf("0x02726D50CD200C195F3D1Cd5349Ca1B7f90D6BB5")
+        await depositContract.redeemTransferWithPayload(
+            signedTransferMessage
+        )
+        const receiverStablecoinBalanceAfter = await stablecoin.balanceOf("0x02726D50CD200C195F3D1Cd5349Ca1B7f90D6BB5");
+
+        console.log("Stablecoin balance before: ", receiverStablecoinBalanceBefore);
+        console.log("Stablecoin balance after: ", receiverStablecoinBalanceAfter);
     })
 })
